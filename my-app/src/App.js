75b391c59cc7b4cdc98c55220235f3515a1fac53
Andrey Lashkevich <andlashkevich@ya.ru>
2024-11-11 19:50:13 +0300

    3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+
+import styles from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className={styles.app}>
+			<fieldset className={styles.container}>
+				<form>
+					<input className={styles.display} type="text" name="display" readonly/>
+
+					<input className={styles.button + ' ' + styles.digit} type="button" value="7"/>
+					<input className={styles.button + ' ' + styles.digit} type="button" value="8"/>
+					<input className={styles.button + ' ' + styles.digit} type="button" value="9"/>
+					<input className={styles.button + ' ' + styles.math} type="button" value="+"/>
+					<br/>
+					<input className={styles.button + ' ' + styles.digit} type="button" value="4"/>
+					<input className={styles.button + ' ' + styles.digit} type="button" value="5"/>
+					<input className={styles.button + ' ' + styles.digit} type="button" value="6"/>
+					<input className={styles.button + ' ' + styles.math} type="button" value="−"/>
+					<br/>
+					<input className={styles.button + ' ' + styles.digit} type="button" value="1"/>
+					<input className={styles.button + ' ' + styles.digit} type="button" value="2"/>
+					<input className={styles.button + ' ' + styles.digit} type="button" value="3"/>
+					<input className={styles.button + ' ' + styles.math} type="button" value="="/>
+					<input className={styles.button + ' ' + styles.digit} type="button" value="0"/>
+					<input className={styles.button + ' ' + styles.clear} type="button" value="C"/>
+
+				</form>
+			</fieldset>
+			<header className={styles['App-header']}></header>
+		</div>
+	);
 }
 
 export default App;
