ebc1d478d276a5c489ba2a63c85adf12b5d34904
Andrey Lashkevich <andlashkevich@ya.ru>
2024-11-12 20:17:38 +0300

    4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,87 @@
-
 import styles from './App.module.css';
+import { useState } from 'react';
 
 function App() {
+	const [op1, setOp1] = useState('');
+	const [op, setOp] = useState('');
+	const [op2, setOp2] = useState('');
+	const [display, setDisplay] = useState(0);
+	const [pass, setPass] = useState(false);
+	const ar = [
+		{ bt: 'digit', value: '7' },
+		{ bt: 'digit', value: '8' },
+		{ bt: 'digit', value: '9' },
+		{ bt: 'math', value: '+' },
+		{ bt: 'digit', value: '4' },
+		{ bt: 'digit', value: '5' },
+		{ bt: 'digit', value: '6' },
+		{ bt: 'math', value: '-' },
+		{ bt: 'digit', value: '1' },
+		{ bt: 'digit', value: '2' },
+		{ bt: 'digit', value: '3' },
+		{ bt: 'total', value: '=' },
+		{ bt: 'digit', value: '0' },
+		{ bt: 'clear', value: 'C' },
+	];
+	const handleDigit = (event) => {
+		!op ? setOp1(op1 + event.target.value) : setOp2(op2 + event.target.value);
+	};
+	const handleMath = (event) => {
+		setOp(event.target.value);
+		if (pass) {
+			setPass(false);
+			setOp1(String(display));
+			setOp2('');
+			setDisplay(0);
+		}
+	};
+	const handleTotal = () => {
+		setPass(true);
+		op === '+' && op1 && op2
+			? setDisplay(Number(op1) + Number(op2))
+			: setDisplay(Number(op1) - Number(op2));
+	};
+	const handleClear = () => {
+		setOp('');
+		setOp1('');
+		setOp2('');
+		setDisplay(0);
+		setPass(false);
+	};
+
 	return (
 		<div className={styles.app}>
 			<fieldset className={styles.container}>
 				<form>
-					<input className={styles.display} type="text" name="display" readonly/>
-
-					<input className={styles.button + ' ' + styles.digit} type="button" value="7"/>
-					<input className={styles.button + ' ' + styles.digit} type="button" value="8"/>
-					<input className={styles.button + ' ' + styles.digit} type="button" value="9"/>
-					<input className={styles.button + ' ' + styles.math} type="button" value="+"/>
-					<br/>
-					<input className={styles.button + ' ' + styles.digit} type="button" value="4"/>
-					<input className={styles.button + ' ' + styles.digit} type="button" value="5"/>
-					<input className={styles.button + ' ' + styles.digit} type="button" value="6"/>
-					<input className={styles.button + ' ' + styles.math} type="button" value="−"/>
-					<br/>
-					<input className={styles.button + ' ' + styles.digit} type="button" value="1"/>
-					<input className={styles.button + ' ' + styles.digit} type="button" value="2"/>
-					<input className={styles.button + ' ' + styles.digit} type="button" value="3"/>
-					<input className={styles.button + ' ' + styles.math} type="button" value="="/>
-					<input className={styles.button + ' ' + styles.digit} type="button" value="0"/>
-					<input className={styles.button + ' ' + styles.clear} type="button" value="C"/>
-
+					<input
+						className={
+							!pass ? styles.display : styles.display + ' ' + styles.result
+						}
+						type="text"
+						name="display"
+						value={pass ? display : op1 + ' ' + op + ' ' + op2}
+						readOnly={true}
+					/>
+					{ar.map((it) => {
+						return (
+							<input
+								onClick={
+									it.bt === 'digit'
+										? handleDigit
+										: it.bt === 'math'
+											? handleMath
+											: it.bt === 'total'
+												? handleTotal
+												: handleClear
+								}
+								className={styles.button + ' ' + styles[`${it.bt}`]}
+								disabled={it.bt === 'math' && !op1 ? true : false}
+								type="button"
+								value={it.value}
+								key={String(Date.now()) + String(Math.random())}
+							/>
+						);
+					})}
 				</form>
 			</fieldset>
 			<header className={styles['App-header']}></header>
